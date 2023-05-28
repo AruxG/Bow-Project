ad68f02cda7586ce89cb576c41f6516b84393733
AntonioRobledinos <48557378+AruxG@users.noreply.github.com>
2023-05-28 21:31:49 +0200

    Initial commit

---
+++ Assets/Architecture/Village/CabinController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CabinController : MonoBehaviour
+{
+
+    [SerializeField] GameObject myicon;
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag.Equals("Player"))
+        {
+            GameObject.FindObjectOfType<ReloadControlController>().lumberIcon = true;
+            myicon.SetActive(true);
+        }
+    }
+}
